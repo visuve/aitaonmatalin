948dda963ee88c8d04ab666dfc216819b6f03da3
visuve <veli-matti.visuri@iki.fi>
2025-10-30 11:37:09 +0200

    Complete refactor
    - Add a class "Game" & "GameConfiguration"
    - Why? I got annoyed because the screen resolution was hardcoded

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,34 +1,97 @@
 namespace aita
 {
-	constexpr float WindowWidth = 1600.0f;
-	constexpr float WindowHeight = 1200.0f;
+	struct GameConfiguration
+	{
+		constexpr static uint32_t FramesPerSecond = 30;
+		constexpr static float DefaultWindowWidth = 800.0f;
+		constexpr static float DefaultWindowHeight = 600.0f;
 
-	constexpr float FenceWidth = WindowWidth / 40.0f;
-	constexpr float FenceHeight = WindowHeight / 3.0f;
-	constexpr float FenceHalf = FenceWidth / 2.0f;
-	constexpr float FenceX = WindowWidth / 2 - FenceHalf;
-	constexpr float FenceY = WindowHeight - FenceHeight;
-	constexpr float FenceLeft = FenceX;
-	constexpr float FenceMiddle = FenceLeft + FenceHalf;
-	constexpr float FenceRight = FenceLeft + FenceWidth;
+		const float WindowWidth;
+		const float WindowHeight;
 
-	float Gravity = WindowHeight / 1200.0f;
-	constexpr float JumpVelocity = -(WindowHeight / 37.5f);
-	constexpr float MoveVelocity = WindowWidth / 160.0f;
-	constexpr float HitPenaltyHorizontal = WindowWidth / 53.33f;
-	constexpr float HitPenaltyVertical = WindowHeight / 60.0f;
+		const float FenceWidth;
+		const float FenceHeight;
+		const float FenceHalf;
+		const float FenceX;
+		const float FenceY;
+		const float FenceLeft;
+		const float FenceMiddle;
+		const float FenceRight;
+
+		float Gravity;
+		const float JumpVelocity;
+		const float MoveVelocity;
+		const float HitPenaltyHorizontal;
+		const float HitPenaltyVertical;
+
+		GameConfiguration(float width, float height) :
+			WindowWidth(width),
+			WindowHeight(height),
+			FenceWidth(WindowWidth / 40.0f),
+			FenceHeight(WindowHeight / 3.0f),
+			FenceHalf(FenceWidth / 2.0f),
+			FenceX(WindowWidth / 2 - FenceHalf),
+			FenceY(WindowHeight - FenceHeight),
+			FenceLeft(FenceX),
+			FenceMiddle(FenceLeft + FenceHalf),
+			FenceRight(FenceLeft + FenceWidth),
+			Gravity(WindowHeight / 1200.0f),
+			JumpVelocity(-(WindowHeight / 37.5f)),
+			MoveVelocity(WindowWidth / 160.0f),
+			HitPenaltyHorizontal(WindowWidth / 53.33f),
+			HitPenaltyVertical(WindowHeight / 60.0f)
+		{
+		}
+	};
+
+	namespace snd
+	{
+		constexpr float LowNote = 50.0f;
+		constexpr float HighNote = 5000.0f;
+		constexpr float NoteStep = 1.5f;
+		constexpr uint32_t NoteDuration = 60;
+
+		void lose()
+		{
+#ifdef WIN32
+			for (float note = HighNote; note >= LowNote; note /= NoteStep)
+			{
+				_beep(static_cast<uint32_t>(note), NoteDuration);
+			}
+			_sleep(500);
+#endif
+		}
+
+		void win()
+		{
+#ifdef WIN32
+			for (float note = LowNote; note <= HighNote; note *= NoteStep)
+			{
+				_beep(static_cast<uint32_t>(note), NoteDuration);
+			}
+			_sleep(500);
+#endif
+		}
+	}
 
 	class Player : public sf::Drawable
 	{
 	public:
-		static constexpr float Radius = (WindowWidth + WindowHeight) / 35.0f;
-		static constexpr float Diameter = Radius * 2.0f;
-		static constexpr float MinimumX = 0.0f;
-		static constexpr float MaximumX = WindowWidth - Diameter;
-		static constexpr float MinimumY = 0.0f;
-		static constexpr float MaximumY = WindowHeight - Diameter;
+		const float Radius;
+		const float Diameter;
+		const float MinimumX;
+		const float MaximumX;
+		const float MinimumY;
+		const float MaximumY;
 
-		Player() :
+		Player(const GameConfiguration& config) :
+			Radius((config.WindowWidth + config.WindowHeight) / 35.0f),
+			Diameter(Radius * 2.0f),
+			MinimumX(0.0f),
+			MaximumX(config.WindowWidth - Diameter),
+			MinimumY(0.0f),
+			MaximumY(config.WindowHeight - Diameter),
+			_config(config),
 			_shape(Radius, 8),
 			_position(MinimumX, MaximumY),
 			_velocity(0.0f, 0.0f)
@@ -37,14 +100,19 @@ namespace aita
 			_shape.setFillColor(sf::Color::Green);
 		}
 
-		~Player() = default;
+		void reset()
+		{
+			_position = { MinimumX, MaximumY };
+			_velocity = { 0.0f, 0.0f };
+			_shape.setPosition(_position);
+		}
 
 		void jump()
 		{
 			// Is on ground level
 			if (_position.y >= MaximumY)
 			{
-				_velocity.y = JumpVelocity;
+				_velocity.y = _config.JumpVelocity;
 			}
 		}
 
@@ -79,7 +147,7 @@ namespace aita
 			else
 			{
 				// Apply gravity
-				_velocity.y += Gravity;
+				_velocity.y += _config.Gravity;
 			}
 
 			_shape.setPosition(_position);
@@ -89,20 +157,20 @@ namespace aita
 			const float playerCenter = _position.x + Radius;
 
 			// Fence collision
-			if (_position.y > FenceY - Diameter && playerRight > FenceLeft && playerLeft < FenceRight)
+			if (_position.y > _config.FenceY - Diameter && playerRight > _config.FenceLeft && playerLeft < _config.FenceRight)
 			{
-				if (playerCenter < FenceMiddle)
+				if (playerCenter < _config.FenceMiddle)
 				{
 					_shape.setFillColor(sf::Color::Blue);
-					_position.x -= HitPenaltyHorizontal;
-					_position.y += HitPenaltyVertical;
+					_position.x -= _config.HitPenaltyHorizontal;
+					_position.y += _config.HitPenaltyVertical;
 				}
 
-				if (playerCenter > FenceMiddle)
+				if (playerCenter > _config.FenceMiddle)
 				{
 					_shape.setFillColor(sf::Color::Magenta);
-					_position.x += HitPenaltyHorizontal;
-					_position.y += HitPenaltyVertical;
+					_position.x += _config.HitPenaltyHorizontal;
+					_position.y += _config.HitPenaltyVertical;
 				}
 			}
 			else
@@ -117,125 +185,137 @@ namespace aita
 			return { _position.x + Diameter, _position.y + Diameter };
 		}
 
-	private:
 		void draw(sf::RenderTarget& target, sf::RenderStates states) const override
 		{
 			target.draw(_shape, states);
 		}
 
+	private:
+		const aita::GameConfiguration& _config;
 		sf::Vector2f _position;
 		sf::Vector2f _velocity;
 		sf::CircleShape _shape;
 	};
 
-	constexpr float LowNote = 50.0f;
-	constexpr float HighNote = 5000.0f;
-	constexpr float NoteStep = 1.5f;
-	constexpr uint32_t NoteDuration = 60;
-
-	void lose()
+	class Game
 	{
-#ifdef WIN32
-		for (float note = HighNote; note >= LowNote; note /= NoteStep)
+	public:
+		Game() = delete;
+
+		Game(float width, float height) :
+			Config(width, height),
+			_player(Config)
 		{
-			_beep(static_cast<uint32_t>(note), NoteDuration);
+			sf::Vector2u resolution(static_cast<uint32_t>(width), static_cast<uint32_t>(height));
+			sf::VideoMode videoMode(resolution, 8);
+
+			_window = sf::RenderWindow(videoMode, "Aita on matalin");
+			_window.setVerticalSyncEnabled(true);
+			_window.setFramerateLimit(Config.FramesPerSecond);
+
+			_fence = sf::RectangleShape({ Config.FenceWidth, Config.FenceHeight });
+			_fence.setFillColor(sf::Color::Red);
+			_fence.setPosition({ Config.FenceX, Config.FenceY });
 		}
-		_sleep(500);
-#endif
-	}
 
-	void win()
-	{
-#ifdef WIN32
-		for (float note = LowNote; note <= HighNote; note *= NoteStep)
+		GameConfiguration Config;
+
+		operator bool() const
 		{
-			_beep(static_cast<uint32_t>(note), NoteDuration);
+			return _window.isOpen();
 		}
-		_sleep(500);
-#endif
-	}
 
-	constexpr uint32_t FramesPerSecond = 30;
-	constexpr int32_t TimeOut = FramesPerSecond * 10;
-
-	int32_t play(sf::RenderWindow& window)
-	{
-		aita::Player player;
-
-		const auto onClose = [&window](const sf::Event::Closed&)
+		int32_t play()
 		{
-			window.close();
-		};
+			const auto handleClose = std::bind(&Game::onClose, this, std::placeholders::_1);
+			const auto handleKeypress = std::bind(&Game::onKeyPressed, this, std::placeholders::_1);
 
-		const auto onKeyPressed = [&window, &player](const sf::Event::KeyPressed& keyPressed)
+			_player.reset();
+
+			// The score is inversely proportional to the time taken to finish the game
+			// Higher score equals less time taken to make the jump
+			int32_t score = GameConfiguration::FramesPerSecond * 10;
+
+			while (_window.isOpen())
+			{
+				_window.handleEvents(handleClose, handleKeypress);
+
+				onMove();
+
+				_player.update();
+
+				_window.clear();
+				_window.draw(_fence);
+				_window.draw(_player);
+				_window.display();
+
+				if (_player.bottomRight().x >= Config.WindowWidth &&
+					_player.bottomRight().y >= Config.WindowHeight)
+				{
+					snd::win();
+					break;
+				}
+
+				if (!--score)
+				{
+					snd::lose();
+					break;
+				}
+			}
+
+			return score;
+		}
+
+	private:
+		void onClose(const sf::Event::Closed& closed)
+		{
+			_window.close();
+		}
+
+		void onKeyPressed(const sf::Event::KeyPressed& keyPressed)
 		{
 			if (keyPressed.scancode == sf::Keyboard::Scancode::Escape)
 			{
-				window.close();
+				_window.close();
 			}
-
 			if (keyPressed.scancode == sf::Keyboard::Scancode::Space)
 			{
-				player.jump();
+				_player.jump();
 			}
-		};
+		}
 
-		sf::RectangleShape fence({ aita::FenceWidth, aita::FenceHeight });
-		fence.setFillColor(sf::Color::Red);
-		fence.setPosition({ aita::FenceX, aita::FenceY });
-
-		int32_t ticks = 0;
-
-		while (window.isOpen())
+		void onMove()
 		{
-			window.handleEvents(onClose, onKeyPressed);
-
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
 			{
-				player.move({ aita::MoveVelocity, 0.0f });
+				_player.move({ Config.MoveVelocity, 0.0f });
 			}
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
 			{
-				player.move({ -aita::MoveVelocity, 0.0f });
+				_player.move({ -Config.MoveVelocity, 0.0f });
 			}
 
-			if (aita::Gravity == 0.0f)
+			if (Config.Gravity > 0.0f)
 			{
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-				{
-					player.move({ 0.0f, -aita::MoveVelocity });
-				}
-
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-				{
-					player.move({ 0.0f, aita::MoveVelocity });
-				}
+				return;
 			}
 
-			player.update();
-
-			window.clear();
-			window.draw(fence);
-			window.draw(player);
-			window.display();
-
-			if (player.bottomRight().x >= aita::WindowWidth &&
-				player.bottomRight().y >= aita::WindowHeight)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
 			{
-				aita::win();
-				break;
+				_player.move({ 0.0f, -Config.MoveVelocity });
 			}
 
-			if (++ticks > 300)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
 			{
-				aita::lose();
-				break;
+				_player.move({ 0.0f, Config.MoveVelocity });
 			}
 		}
 
-		return TimeOut - ticks;
-	}
+		Player _player;
+		sf::RenderWindow _window;
+		sf::RectangleShape _fence;
+	};
 
 	class Arguments
 	{
@@ -245,9 +325,22 @@ namespace aita
 		{
 		}
 
-		std::string_view operator[](std::string_view flag)
+		template <typename T>
+		T get(std::string_view flag, T defaultValue) const
 		{
-			return find(flag);
+			const std::string& value = find(flag);
+
+			if (value == "not found")
+			{
+				return defaultValue;
+			}
+
+			if constexpr (std::is_same_v<T, float>)
+			{
+				return std::stof(value);
+			}
+
+			static_assert("Unsupported type");
 		}
 
 		bool contains(const std::string_view flag) const
@@ -256,9 +349,9 @@ namespace aita
 		}
 
 	private:
-		std::string_view find(std::string_view flag) const
+		std::string find(std::string_view flag) const
 		{
-			for (std::string_view argument : _arguments)
+			for (const std::string& argument : _arguments)
 			{
 				if (!argument.starts_with(flag))
 				{
@@ -286,9 +379,25 @@ int main(int argc, char** argv)
 {
 	aita::Arguments arguments(argc, argv);
 
+	if (arguments.contains("--help") || arguments.contains("-h"))
+	{
+		std::println("aitaonmatalin - an SFML + LibTorch example game");
+		std::println("\noptions:");
+		std::println("\t--no-gravity     Disable gravity");
+		std::println("\t--width=<value>  Set the window width (default: {})", aita::GameConfiguration::DefaultWindowWidth);
+		std::println("\t--height=<value> Set the window height (default: {})", aita::GameConfiguration::DefaultWindowHeight);
+		std::println("\t--loop           Run the game in a loop");
+		return 0;
+	}
+
+	const float windowWidth = arguments.get<float>("--width", aita::GameConfiguration::DefaultWindowWidth);
+	const float windowHeight = arguments.get<float>("--height", aita::GameConfiguration::DefaultWindowHeight);
+
+	aita::Game game(windowWidth, windowHeight);
+
 	if (arguments.contains("--no-gravity"))
 	{
-		aita::Gravity = 0.0f;
+		game.Config.Gravity = 0.0f;
 		std::println("Gravity disabled");
 	}
 
@@ -296,21 +405,11 @@ int main(int argc, char** argv)
 	torch::Tensor tensor(torch::rand({ 3, 3 }, device));
 	std::cout << tensor << std::endl;
 
-	sf::VideoMode videoMode(
+	do
 	{
-		static_cast<uint32_t>(aita::WindowWidth),
-		static_cast<uint32_t>(aita::WindowHeight)
-	}, 8);
+		std::println("Score: {}", game.play());
 
-	sf::RenderWindow window(videoMode, "Aita on matalin");
-	window.setVerticalSyncEnabled(true);
-	window.setFramerateLimit(aita::FramesPerSecond);
-
-	do 
-	{
-		std::println("Score: {}", aita::play(window));
-
-	} while (window.isOpen() && arguments.contains("--loop"));
+	} while (game && arguments.contains("--loop"));
 
 	return 0;
 }
