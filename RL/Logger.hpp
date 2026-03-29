20e2b8420993d208cab6c3f91e6b01bd1a3119ab
visuve <veli-matti.visuri@iki.fi>
2026-03-29 20:12:28 +0300

    Improve the Logger

--- RL/Logger.hpp
+++ RL/Logger.hpp
@@ -13,62 +13,48 @@ namespace aita
 
 		Logger(const Logger&) = delete;
 		Logger& operator=(const Logger&) = delete;
+		Logger(Logger&&) = delete;
+		Logger& operator=(Logger&&) = delete;
 
-#if _DEBUG
-		template <typename... Args>
-		void logDebug(std::format_string<Args...> fmt, Args&&... args)
+		template <char Level, typename... Args>
+		void log(std::format_string<Args...> fmt, Args&&... args)
 		{
 			std::scoped_lock lock(_mutex);
-			logTo(std::cout, 'D', fmt, std::forward<Args>(args)...);
-		}
-#else
-		template <typename... Args>
-		void logDebug(std::format_string<Args...>, Args&&...)
-		{
-		}
-#endif
 
-		template <typename... Args>
-		void logInfo(std::format_string<Args...> fmt, Args&&... args)
-		{
-			std::scoped_lock lock(_mutex);
-			logTo(std::cout, 'I', fmt, std::forward<Args>(args)...);
-		}
+			auto& stream = []() -> std::ostream&
+			{
+				if constexpr (Level == 'E' || Level == 'W')
+				{
+					return std::cerr;
+				}
 
-		template <typename... Args>
-		void logWarning(std::format_string<Args...> fmt, Args&&... args)
-		{
-			std::scoped_lock lock(_mutex);
-			logTo(std::cerr, 'W', fmt, std::forward<Args>(args)...);
-		}
+				return std::cout;
+			}();
 
-		template <typename... Args>
-		void logError(std::format_string<Args...> fmt, Args&&... args)
-		{
-			std::scoped_lock lock(_mutex);
-			logTo(std::cerr, 'E', fmt, std::forward<Args>(args)...);
+			const auto now = std::chrono::floor<std::chrono::milliseconds>(std::chrono::system_clock::now());
+			std::print(stream, "[{:%FT%TZ}][{}] ", now, Level);
+			std::println(stream, fmt, std::forward<Args>(args)...);
 		}
 
 	private:
 		Logger() = default;
 		std::mutex _mutex;
-
-		template <typename... Args>
-		void logTo(std::ostream& stream, char level, std::format_string<Args...> fmt, Args&&... args)
-		{
-			auto now = std::chrono::floor<std::chrono::milliseconds>(std::chrono::system_clock::now());
-			std::print(stream, "[{:%FT%TZ}][{}] ", now, level);
-			std::println(stream, fmt, std::forward<Args>(args)...);
-		}
 	};
 
 	inline constexpr struct
 	{
+#ifndef NDEBUG
 		template <typename... Args>
 		void operator()(std::format_string<Args...> fmt, Args&&... args) const
 		{
-			Logger::instance().logDebug(fmt, std::forward<Args>(args)...);
+			Logger::instance().log<'D'>(fmt, std::forward<Args>(args)...);
 		}
+#else
+		template <typename... Args>
+		void operator()(std::format_string<Args...>, Args&&...) const
+		{
+		}
+#endif
 	} LOGD;
 
 	inline constexpr struct
@@ -76,7 +62,7 @@ namespace aita
 		template <typename... Args>
 		void operator()(std::format_string<Args...> fmt, Args&&... args) const 
 		{
-			Logger::instance().logInfo(fmt, std::forward<Args>(args)...);
+			Logger::instance().log<'I'>(fmt, std::forward<Args>(args)...);
 		}
 	} LOGI;
 
@@ -85,7 +71,7 @@ namespace aita
 		template <typename... Args>
 		void operator()(std::format_string<Args...> fmt, Args&&... args) const
 		{
-			Logger::instance().logWarning(fmt, std::forward<Args>(args)...);
+			Logger::instance().log<'W'>(fmt, std::forward<Args>(args)...);
 		}
 	} LOGW;
 
@@ -94,8 +80,7 @@ namespace aita
 		template <typename... Args>
 		void operator()(std::format_string<Args...> fmt, Args&&... args) const 
 		{
-			Logger::instance().logError(fmt, std::forward<Args>(args)...);
+			Logger::instance().log<'E'>(fmt, std::forward<Args>(args)...);
 		}
 	} LOGE;
-
 }
